44eaff7069d0466c437e2d5f3c9799740c9e476f
Abhishek-90 <holaniabhishek90@gmail.com>
2022-07-11 00:23:37 +0530

    Router Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import classes from './App.module.css';
+import classes from "./App.module.css";
 import User from "./Components/User/User.js";
 import AdminLogin from "./Components/Admin/AdminLogin.js";
+import { Routes,Route, NavLink } from "react-router-dom";
+
 function App() {
   return (
-    <div className={classes.container}>
-      {/* <User />       */}
-      <AdminLogin/>
-    </div>
+    <>
+      <NavLink className={classes.link} to="/admin">Admin</NavLink>
+      <NavLink className={classes.link} to="/user">User</NavLink>
+      <div className={classes.container}>
+        <Routes>
+          <Route exact path="/admin" element={<AdminLogin />} />
+          <Route exact path="/user" element={<User />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
